28ade924401983b4137019d663485eb409bbde24
Tanmay Singh <tanmaysinghcg@gmail.com>
2023-08-17 16:27:05 +0530

    Third commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,30 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'models/catalog.dart';
+import 'widgets/drawer.dart';
+
+import 'package:new_app/models/catalog.dart';
 import 'package:new_app/widgets/drawer.dart';
 import 'package:new_app/widgets/item_widgets.dart';
 
-import 'models/catalog.dart';
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  final String name = "Codepur";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+    await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text("Catalog App", style: TextStyle(color: Colors.black),),
+        title: Text("Catalog App", style: TextStyle(color: Colors.black),),
       ),
-        body:  Padding(
-    padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
           itemCount: CatalogModel.items.length,
-            itemBuilder: (context, index){
-              return ItemWidget(
-                item: CatalogModel.items[index],
-              );
-            },
+          itemBuilder: (context, index) => ItemWidget(
+            item: CatalogModel.items[index],
+          ),
+        )
+            : Center(
+          child: CircularProgressIndicator(),
         ),
-        ),
+      ),
       drawer: MyDrawer(),
     );
   }
--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'lib/models/catalog.dart';
 
 class CatalogModel {
-  static final items = [
+  static List<Item> items = [
     Item(
-      id: "tan01",
+      id: 1,
       name: "iPhone 14 Pro",
       desc: "APPLE iPhone 14 Pro (Silver, 128 GB)",
       price: 119999,
@@ -14,7 +15,7 @@ class CatalogModel {
   ];
 }
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -22,5 +23,24 @@ class Item {
   final String image;
 
   Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+
+  factory Item.fromMap(Map<String,dynamic> map)=> Item(
+      id: map["id"],
+      name: map["name"],
+      desc: map["desc"],
+      price: map["price"],
+      color: map["color"],
+      image: map["image"],
+
+    );
+
+  toMap() => {
+    "id" : id,
+    "name" : name,
+    "desc": desc,
+    "price" : price,
+    "color" : color,
+    "image" : image
+  };
 }
 
